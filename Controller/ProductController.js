54ac8185e38e6f7fc2d0176975fbd7a7b24ff0ca
MadhavT-1 <62096719+MadhavT-1@users.noreply.github.com>
2022-10-17 13:46:47 +0530

    Products api completed

--- Controller/ProductController.js
+++ Controller/ProductController.js
@@ -16,15 +16,44 @@ module.exports.getAllProducts = (req, res) => {
 }
 
 module.exports.getProductCategories = (req, res) => {
-    res.send("getProductCategories working")
+    Product.distinct('category')
+        .then((products) => {
+            res.status(200).send(products)
+        })
+        .catch((err) => {
+            res.status(404).json({ message: 'Not found' })
+        })
 }
 
 module.exports.getProductsInCategory = (req, res) => {
-    res.send("getProductsInCategory working")
+    const _category = req.params.category
+    const limit = Number(req.query.limit) || 0;
+    const sort = req.query.sort == 'desc' ? -1 : 1;
+
+    Product.find({ category: _category })
+        .select(['-_id', '-__v'])       //it wont return this values 
+        .limit(limit)
+        .sort({ id: sort })
+        .then((products) => {
+            res.status(200).json({ Products: products })
+        })
+        .catch(() => {
+            res.status(404).json({ message: "not found" })
+        })
+
 }
 
 module.exports.getProduct = (req, res) => {
-    res.send("getProduct working")
+    const productId = req.params.id;
+
+    Product.findOne({ id: productId })
+        .select(['-_id', '-__v'])
+        .then((product) => {
+            res.status(200).json({ product })
+        })
+        .catch((err) => {
+            res.status(404).json({ message: "Product Not Found" })
+        })
 }
 
 module.exports.addProduct = (req, res) => {
@@ -52,8 +81,36 @@ module.exports.addProduct = (req, res) => {
 }
 
 module.exports.editProduct = (req, res) => {
-    res.send("editProduct working")
+    const prodId = req.params.id;
+    const prod = req.body
+    if (prod === undefined || prod === null) {
+        res.status(500).send("Data is undefined")
+    }
+    const updateProduct = {
+
+        title: prod.title,
+        price: prod.price,
+        description: prod.description,
+        image: prod.image,
+        category: prod.category
+    }
+
+    Product.updateOne({ id: prodId }, updateProduct)
+        .then(() => {
+            res.status(201).json({ message: "Product Updated" })
+        })
+        .catch((err) => {
+            res.status(409).json({ message: "Error in update " + err.message })
+        })
 }
 module.exports.deleteProduct = (req, res) => {
-    res.send("deleteProduct working")
+    const prodId = req.params.id;
+
+    Product.deleteOne({ id: prodId })
+        .then(() => {
+            res.status(200).json({ message: "Product Deleted" })
+        })
+        .catch((err) => {
+            res.status(401).json({ message: "Error in delete :" + err.message })
+        })
 }
